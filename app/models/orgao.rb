1bd2c39b5ca9894e461367001b0cb42d3bf837a2
Alexandre Gomes <alegomes@gmail.com>
2012-02-18 11:24:37 -0200

    orgao has many links

--- app/models/orgao.rb
+++ app/models/orgao.rb
@@ -1,4 +1,5 @@
 class Orgao < ActiveRecord::Base
   belongs_to :siorg
   belongs_to :licenca
+  has_many :links
 end
--- spec/models/orgao_spec.rb
+++ spec/models/orgao_spec.rb
@@ -3,4 +3,5 @@ require 'spec_helper'
 describe Orgao do
 	it 'belongs to SIORG'
 	it 'has one licence'
+	it 'has many links'
 end
