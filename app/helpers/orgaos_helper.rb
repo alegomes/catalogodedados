01a19f7e150b5b02f3df662986e3213cba403dea
Alexandre Gomes <alegomes@gmail.com>
2012-02-27 09:05:28 -0300

    remocao de Nested Link do Orgao funcionando. Vamos ver se ele aceita valores em branco

--- app/helpers/orgaos_helper.rb
+++ app/helpers/orgaos_helper.rb
@@ -1,9 +1,16 @@
 module OrgaosHelper
+	
+	# From https://github.com/ryanb/railscasts-episodes/blob/master/episode-197/surveysays/app/helpers/application_helper.rb
 	def link_to_add_links(name, f, association)
     new_object = f.object.class.reflect_on_association(association).klass.new
     fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
       render(association.to_s, :f => builder)
     end
     link_to_function(name, "add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")")
+  end
+
+	# From https://github.com/ryanb/railscasts-episodes/blob/master/episode-197/surveysays/app/helpers/application_helper.rb
+  def link_to_remove_fields(name, f)
+    f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)")
   end
 end
