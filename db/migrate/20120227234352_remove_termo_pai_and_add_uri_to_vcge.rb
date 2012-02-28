e477a9da462fef8e2094568abb17d5c775995b20
Alexandre Gomes <alegomes@gmail.com>
2012-02-27 23:27:39 -0300

    troquei t.rename por t.remove + t.integer pra ver se resolve o problema de undefined method uri for #<Vcge:0x00000004f4d798>

--- db/migrate/20120227234352_remove_termo_pai_and_add_uri_to_vcge.rb
+++ db/migrate/20120227234352_remove_termo_pai_and_add_uri_to_vcge.rb
@@ -2,14 +2,16 @@ class RemoveTermoPaiAndAddUriToVcge < ActiveRecord::Migration
   def up
 		change_table :vcges do |t|
 		  t.remove :termo_pai_id
-		  t.rename :termo, :uri
+		  t.remove :termo
+		  t.string :uri
 		end
   end
 
   def down
 		change_table :vcges do |t|
 		  t.integer :termo_pai_id
-		  t.rename :uri, :termo
+		  t.string  :termo
+		  t.remove  :uri
 		end
 
   end
