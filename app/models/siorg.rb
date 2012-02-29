d1e23813f3f81a6c9f371f66b6544607bce0f6e2
Alexandre Gomes <alegomes@gmail.com>
2012-02-29 18:15:39 -0300

    inclusao de toda a hierarquia de siorgs na busca pelo nome apos preenchimento do codigo

--- app/models/siorg.rb
+++ app/models/siorg.rb
@@ -1,2 +1,21 @@
 class Siorg < ActiveRecord::Base
+	belongs_to :siorg_pai, :class_name => 'Siorg', :primary_key => :codigo, :foreign_key => :codigo_pai
+	has_many :siorg_filhos, :class_name => 'Siorg', :primary_key => :codigo, :foreign_key => :codigo_pai, :order => 'codigo_pai ASC'
+	
+	def hierarquia()
+		stack = []
+		stack.push(self.nome)
+		
+		siorg = self
+		while siorg != siorg.siorg_pai
+			stack.push(siorg.siorg_pai.nome)
+			siorg = siorg.siorg_pai
+		end
+		
+		array = []
+		array << stack.pop while not stack.empty?
+		array
+	end
+	
+	
 end
